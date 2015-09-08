fd1ed9564f2a1b6764eb6f112ccdf28cc04168e2
andrewbr <ajbryant.01@gmail.com>
2015-09-08 11:46:36 +1000

    footer changes

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -4,19 +4,19 @@
           <div class='footer-links-column'>
             <h4 class='footer-links-title'>Social</h4>
             <ul class='footer-links-list'>
-              <li><a href="//getflow.com">Twatter</a></li>
-              <li><a href="//getballpark.com">Dribbley</a></li>
-              <li><a href="//usepeak.com">LunkedIn</a></li>
-              <li><a href="//pixelunion.net">Pixelface</a></li>
+              <li><a href="#">Twatter</a></li>
+              <li><a href="#">Dribbley</a></li>
+              <li><a href="#">LunkedIn</a></li>
+              <li><a href="#">Pixelface</a></li>
             </ul>
           </div>
            <div class='footer-links-column'>
             <h4 class='footer-links-title'>Journal</h4>
             <ul class='footer-links-list'>
-              <li><a href="//getflow.com">Entry 1</a></li>
-              <li><a href="//getballpark.com">Entry 2</a></li>
-              <li><a href="//usepeak.com">Entry 3</a></li>
-              <li><a href="//pixelunion.net">Entry 4</a></li>
+              <li><a href="#">Entry 1</a></li>
+              <li><a href="#">Entry 2</a></li>
+              <li><a href="#">Entry 3</a></li>
+              <li><a href="#">Entry 4</a></li>
             </ul>
           </div>
           <div class='footer-links-column'>
@@ -24,7 +24,7 @@
             <ul class='footer-links-list'>
               <li><a href="//twitter.com/andrewBryant_">Twitter</a></li>
               <li><a href="#">Facebook</a></li>
-              <li><a href="/writing/">Blog</a></li>
+              <li><a href="/writing/">Journal</a></li>
             </ul>
           </div>
         </div>
