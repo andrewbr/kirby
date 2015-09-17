cf306ff90d73b4cf72f3c836d92da1d6140af12f
andrewbr <ajbryant.01@gmail.com>
2015-09-17 18:01:12 +1000

    default template update

--- site/templates/default.php
+++ site/templates/default.php
@@ -1,16 +1,26 @@
 <?php snippet('header') ?>
 
- <div id='main' role='main'>
-<div class='position top-bg' id='top-bg'>
-        <div class='post-inner-container container'>
-          <div class='post-inner'>
-    <div class="text">
-      <h1><?php echo $page->title()->html() ?></h1>
+<div id='main' role='main'>
+      <div class='position top-bg-grey' id='top-bg-grey'>
+        <div class='position-inner-container container'>
+          <div class='position-inner'>
+            <div class='project-wrapper'>
+              <h1 class='header-title'>
+                 <?php echo $page->title()->html() ?>
+
+              </h1>
+           
+      <div class="text">
       <?php echo $page->text()->kirbytext() ?>
-    </div>
+  
+        
+            </div>
+           
+      </div>
+        </div>
 </div>
-</div>
-</div>
-  </div>
+
+
+
 
 <?php snippet('footer') ?>
